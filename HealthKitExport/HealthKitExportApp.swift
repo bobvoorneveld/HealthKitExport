7b6c4951950dfcfc04b1c0d64c00c58382d2383f
Bob Voorneveld <info@bobvoorneveld.nl>
2023-06-07 12:44:54 +0200

    Initial Commit

---
+++ HealthKitExport/HealthKitExportApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthKitExportApp.swift
+//  HealthKitExport
+//
+//  Created by Bob Voorneveld on 07/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct HealthKitExportApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
